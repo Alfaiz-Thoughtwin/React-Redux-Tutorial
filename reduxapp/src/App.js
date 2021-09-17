0e04b28a4a9235fedf869257e589c90a928b59fd
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-09-17 18:34:37 +0530

    Counter Project via Redux COMPLETED!

--- reduxapp/src/App.js
+++ reduxapp/src/App.js
@@ -1,64 +1,37 @@
-// useReducer()
-// import React, { useReducer } from 'react';
-// import './App.css';
-
-// const initialState = 0;
-
-// const countReducer = (state, action) => {
-//   // console.log(state, action);
-//   if (action.type === 'DECREMENT') {
-//     return state - 1;
-//   }
-//   if (action.type === 'INCREMENT') {
-//     return state + 1;
-//   }
-//   return state;
-// }
-
-// const App = () => {
-
-//   const [state, dispatch] = useReducer(countReducer, initialState)
-
-//   const decHandler = (event) => {
-//     event.preventDefault();
-//     dispatch({
-//       type: "DECREMENT"
-//     });
-//   }
-
-//   const incHandler = (event) => {
-//     event.preventDefault();
-//     dispatch({
-//       type: "INCREMENT"
-//     });
-//   }
-
-//   return (
-//     <>
-//       <div className="container d-flex center__css">
-//         <h1>Increment/Decrement Counter</h1>
-//         <h4>Using React and Redux</h4>
-//       </div>
-//       <div className="container d-flex center__css_2">
-//         <a className="btn anchor__css" title="Decrement" href="/" onClick={decHandler}><span>-</span></a>
-//         <input type="text" className="form-control mx-4 text-center fs-4" value={state} onChange={(event) => event.target.value}/>
-//         <a className="btn anchor__css" title="Increment" href="/" onClick={incHandler}><span>+</span></a>
-//       </div>
-//       <div className="container text-center mt-4">
-//         <p>Decrement by 1 & Increment By 1</p>
-//       </div>
-//     </>
-//   )
-// }
-
-// export default App;
-
 import React from 'react';
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { incNumber, decNumber } from './actions/index';
 
 const App = () => {
+
+  const myState = useSelector((state) => state.changeNumber);
+  const dispatch = useDispatch();
+
+  const decHandler = (event) => {
+    event.preventDefault();
+    dispatch(decNumber());
+  }
+
+  const incHandler = (event) => {
+    event.preventDefault();
+    dispatch(incNumber(5));
+  }
+
   return (
     <>
-      <h1 className="text-center" >Welcome To React Redux</h1>
+      <div className="container d-flex center__css">
+        <h1>Increment/Decrement Counter</h1>
+        <h4>Using React and Redux</h4>
+      </div>
+      <div className="container d-flex center__css_2">
+        <a className="btn anchor__css" onClick={decHandler} title="Decrement" href="/"><span>-</span></a>
+        <input type="text" name="quantity" className="form-control mx-4 text-center fs-4" value={myState} onChange={(event) => event.target.value}/>
+        <a className="btn anchor__css" onClick={incHandler}title="Increment" href="/"><span>+</span></a>
+      </div>
+      <div className="container text-center mt-4">
+        <p>Decrement by 1 & Increment By 5</p>
+      </div>
     </>
   )
 }
